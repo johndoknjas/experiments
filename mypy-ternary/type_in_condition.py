4018cd244981804352c79da0a71b9a08fe417f37
johndoknjas <jdoknjas@sfu.ca>
2023-08-20 01:01:26 -0700

    Using isinstance fixes mypy issue, since it includes subclasses.

--- mypy-ternary/type_in_condition.py
+++ mypy-ternary/type_in_condition.py
@@ -1,10 +1,10 @@
 from typing import Union
 
 def if_statement_example(param: Union[str, int]) -> bool:
-    if type(param) is str:
+    if isinstance(param, str):
         return len(param) == 2
     else:
         return param < 3
     
 def ternary_example(param: Union[str, int]) -> bool:
-    return len(param) == 2 if type(param) is str else param < 3
+    return len(param) == 2 if isinstance(param, str) else param < 3
