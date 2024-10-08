6b3a3cdc7704c93f518c0299f2cddf76010c65c6
johndoknjas <jdoknjas@sfu.ca>
2024-10-08 14:15:14 -0700

    Test whether instance methods are marked as unused.

---
+++ vulture-test/instance-methods.py
@@ -0,0 +1,16 @@
+class MyClass:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def clssMethod():
+        pass
+
+    def instMethod(self):
+        self.instMethod()
+
+def Rec():
+    pass
+    #MyClass.Rec()
+    e = MyClass()
+    print(e)
